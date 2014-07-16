2db36458f96ce1f7fcac2588b88889bba865d83a
bustta <bustta80980@gmail.com>
2014-07-16 23:55:14 +0800

    refactor _header and style cases

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def isSigninFromFB(user)
+    @porvider = user.provider
+    if @provider != nil && @provider.downcase == 'facebook'
+      return true
+    end
+    return false
+  end
 end
