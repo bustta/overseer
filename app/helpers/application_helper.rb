6cedb4f782728d17fb79b14b5eebda53ca63459b
bustta <bustta80980@gmail.com>
2014-07-23 00:54:46 +0800

    lightly modified view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-  def isSigninFromFB(user)
-    @porvider = user.provider
-    if !@provider.nil? && @provider.downcase == 'facebook'
-      return true
-    end
-    return false
-  end
-
   def notice_message
     alert_types = { :notice => :success, :alert => :danger }
 
