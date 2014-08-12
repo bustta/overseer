be99823692fa524851d90217ab304f0ff9b92eb3
bustta <bustta80980@gmail.com>
2014-08-12 23:35:28 +0800

    case status helper and fixed map

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -1,2 +1,14 @@
 module CasesHelper
+  def tabForNewCase(thisCase)
+    if (thisCase.CaseStart.to_date == DateTime.now.to_date )
+      content_tag(:button, 'New', :class => 'btn btn-danger btn-xs')
+    end
+  end
+
+  def tabOnDigging(thisCase)
+    if (DateTime.now >= thisCase.CaseStart && DateTime.now <= thisCase.CaseEnd)
+      content_tag(:button, 'Digging', :class => 'btn btn-warning btn-xs')
+    end
+
+  end
 end
